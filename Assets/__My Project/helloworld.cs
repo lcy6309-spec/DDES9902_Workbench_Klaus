2d5421019ba56cea3772bc47cead90387c14c30f
lcy6309-spec <231804135+lcy6309-spec@users.noreply.github.com>
2025-10-08 17:39:56 +1100

    update
    update

---
+++ Assets/__My Project/helloworld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello world ***");    
+        //Hello this is internal documentationthat is really useful :D
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
